3369bc4bd5ebfaf509b866d6d93839a9c87bffea
Vietor Liu <vietor.liu@gmail.com>
2013-08-20 12:57:56 +0800

    add: JsonMapper for replace LitJson

---
+++ JsonCsharp/JsonCsharp/JsonMapper.cs
@@ -0,0 +1,21 @@
+﻿
+namespace org.vxwo.csharp.json
+{
+    public class JsonMapper
+    {
+        public static T ToObject<T>(string json)
+        {
+            return JsonWriter.Write<T>(JsonReader.Read(json));
+        }
+
+        public static string ToJson(object obj)
+        {
+            return JsonWriter.Write(JsonReader.Read(obj));
+        }
+
+        public static string ToJsonShrink(object obj)
+        {
+            return JsonWriter.WriteShrink(JsonReader.Read(obj));
+        }
+    }
+}
